fc645e2519c5bea5768973a0c959345b38f3bd0c
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-12 11:59:29 +0530

    feat: add home and dashboard components with routing
    - Implemented routing for Home and Dashboard components in app.routes.ts
    - Created Dashboard component with basic template and styles
    - Developed Home component with a visually appealing layout and navigation
    - Added global styles for consistent appearance across the application
    - Included unit tests for the Dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './page/dashboard/dashboard';
+import { Home } from './page/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:"home",
+        component:Home
+    },
+    {
+        path:"dashboard",
+        component:Dashboard
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./page/home/home";
+import { Dashboard } from './page/dashboard/dashboard';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,Dashboard,RouterOutlet, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
