3bb5c971abe08d1afb79d3a82037b99e791c54cf
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-17 14:05:57 +0530

    feat: update routing configuration to include additional home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,20 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './page/dashboard/dashboard';
 import { Home } from './page/home/home';
 import { CustomerManage } from './page/customer-manage/customer-manage';
+import { Component } from '@angular/core';
 
 
 export const routes: Routes = [
     {
+        path:"",
+        component:Home
+    },
+    {
         path:"home",
+        component:Home
+    },
+    {
+        path:"",
         component:Home
     },
     {
